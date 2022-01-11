3a00ca198e204f37636c08010af2530f83edc162
Mendy <mendyw94@gmail.com>
2022-01-10 21:46:34 -0500

    commit tree 6be51c720d77c067d44f7b9ac69d4cce7f6a0fef

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(recArray){
+    const yearWon = recArray.find( (iteration) => {
+        if(iteration.result === "W"){
+            return iteration;
+        }
+    })
+//console.log(yearWon);
+    if (yearWon ) {
+      return yearWon.year;
+    } else {
+      return undefined;
+    }
+}
+
